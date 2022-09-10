5de74b5b00a818034232bb32c281e7f2cd595c79
Ahmed <ahm1se@gmail.com>
2022-09-10 15:58:55 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 
 
 /**
- *
  *  * main - Entry point
  *
  *   *
  *
  *    * Return: Always 0 (Success)
  *
- *     */
+ *
+ */
 
 int main(void)
 
@@ -18,8 +18,6 @@ int main(void)
 
 	 puts("\"Programming is like building a multilingual puzzle");
 
-	  return (0);
+	return (0);
 
 }
-
-
