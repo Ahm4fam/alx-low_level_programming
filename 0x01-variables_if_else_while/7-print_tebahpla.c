c95f10f06f8d2a3918e47c836647f006d7785944
Ahmed <ahm1se@gmail.com>
2022-09-11 15:21:11 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/*
+ * This is the solutin for task 7. prints the lowercase alphabet in reverse
+ *
+ * Auth: Ahmed S Endrie
+ *
+ */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success
+ *
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'z'; alp >= 'a'; alp--)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
