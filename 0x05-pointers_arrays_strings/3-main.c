b5f1f8faab22857f42cf9e8a8482c0d3e275cd80
Ahmed <ahm1se@gmail.com>
2022-09-19 14:27:01 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * This is the solutin for project '0x05. C - Pointers, arrays and strings'
+ * task 3
+ *
+ * Auth: Ahmed S Endrie
+ *
+ */
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success
+ *
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
