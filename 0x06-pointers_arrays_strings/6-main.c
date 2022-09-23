3db9500e3cf368dccaa88c886866483c94502382
Ahmed <ahm1se@gmail.com>
2022-09-22 18:10:10 -0700

    6.Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * This is the solutin for project '0x06. C- More pointers, arrays and strings'
+ * task 6
+ *
+ * Auth: Ahmed S Endrie
+ *
+ */
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char *ptr;
+
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
