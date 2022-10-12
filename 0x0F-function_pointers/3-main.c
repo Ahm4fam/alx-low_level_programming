5203192215f7bc7dded27afbdc71fa2f8eb9a997
Ahmed <ahm1se@gmail.com>
2022-10-12 12:46:03 -0700

    3. performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Invalid\n");
+		printf("Error\n");
 		exit(98);
 	}
 	a = atoi(argv[1]);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -53,7 +53,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error: divided by 0 \n");
+		printf("Error\n");
 		exit(100);
 	}
 	return (a / b);
@@ -71,7 +71,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error: Divided by 0\n");
+		printf("Error\n");
 		exit(100);
 	}
 	return (a % b);
